fb4010bd68db4b267458fd592a1b3448aa021b44
Kimberly Cable <cableredi@comcast.net>
2020-06-17 10:59:39 -0600

    updated for production

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Added user authentification: Only allow comments when user is logged in
 ## Screenshot
 ![Screenshot](./images/videogameblog.PNG)
 
+## Live Site
+[https://videogameblog.vercel.app/](https://videogameblog.vercel.app/)
+
 ## Technology Used:
 HTML5, SCSS/CSS, Node.js, React, Express, PostgreSQL
 
--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,12 @@
 export default {
-    API_ENDPOINT_VIDEOS: `http://localhost:8000/api/videos`,
-    API_ENDPOINT_COMMENTS: `http://localhost:8000/api/comments`,
-    API_ENDPOINT_AUTH: `http://localhost:8000/api/auth`,
-    API_ENDPOINT_USERS: `http://localhost:8000/api/users`,
+    //API_ENDPOINT_VIDEOS: `http://localhost:8000/api/videos`,
+    //API_ENDPOINT_COMMENTS: `http://localhost:8000/api/comments`,
+    //API_ENDPOINT_AUTH: `http://localhost:8000/api/auth`,
+    //API_ENDPOINT_USERS: `http://localhost:8000/api/users`,
+    API_ENDPOINT_VIDEOS: `https://serene-coast-64109.herokuapp.com/api/videos`,
+    API_ENDPOINT_COMMENTS: `https://serene-coast-64109.herokuapp.com/api/comments`,
+    API_ENDPOINT_AUTH: `https://serene-coast-64109.herokuapp.com/api/auth`,
+    API_ENDPOINT_USERS: `https://serene-coast-64109.herokuapp.com/api/users`,
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'videos-blog-client-auth-token',
   }
